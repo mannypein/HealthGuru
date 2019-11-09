5300c895446e1bca7607383494dda5e8ec99642c
Jmart79 <justinjustinjust111@gmail.com>
2019-11-08 19:09:26 -0700

    Updated Models package, now has stubbed out classes and newly created MealGenerator class.

--- health-guru/src/main/java/models/WorkOut.java
+++ health-guru/src/main/java/models/WorkOut.java
@@ -1,5 +1,51 @@
 package models;
 
+import enums.WorkOutType;
+
 public class WorkOut {
 
+	private String name;
+	private long duration;
+	private WorkOutType type;
+	private int sets;
+	
+	public WorkOut() {
+		
+	}
+	
+	public WorkOut(String name, long duration, WorkOutType type, int sets) {
+		setName(name);
+		setDuration(duration);
+		setType(type);
+		setSets(sets);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getDuration() {
+		return duration;
+	}
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+	public WorkOutType getType() {
+		return type;
+	}
+	public void setType(WorkOutType type) {
+		this.type = type;
+	}
+	public int getSets() {
+		return sets;
+	}
+	public void setSets(int sets) {
+		this.sets = sets;
+	}
+	
+	
+	
+	
 }
