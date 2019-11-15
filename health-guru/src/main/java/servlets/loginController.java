75ef73d344aac4feb404bdcf8b3b2caf5102c3ee
Jmart79 <justinjustinjust111@gmail.com>
2019-11-14 23:40:40 -0700

    temp save hopefully nothing messes up

---
+++ health-guru/src/main/java/servlets/loginController.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class loginController extends HttpServlet {
+	private Map<String,String> users = new HashMap();
+	
+	private static final long serialVersionUID = 1L;
+	
+    public loginController() {
+    	
+    }
+    
+    private void fillMap() {
+    	
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
